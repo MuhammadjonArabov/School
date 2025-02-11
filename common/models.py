80348cfbf747cd2802530b02610ff85d4b0ac7d4
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-02-11 17:43:10 +0500

    update models

--- common/models.py
+++ common/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -7,6 +11,27 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-class Category(BaseModel):
-    subject = models.CharField(max_length=255)
-    class_name = models.CharField(max_length=255)
+
+class Subjects(BaseModel):
+    ''' Subjects names in school '''
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class ClassName(BaseModel):
+    ''' class name in school '''
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Teacher(BaseModel):
+    ''' teachers working at the school '''
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.full_name} | {self.position}"
+
