862c48af92e44c8f0b525925b96e55c517cfcc25
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-02-14 16:54:23 +0500

    update

--- common/urls.py
+++ common/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('active-student/', views.active_student_list, name='active_student_list'),
+
 ]
--- common/views.py
+++ common/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from . import models
 
 def main(request):
     return render(request, 'base.html')
+
+def active_student_list(request):
+    students = models.ActiveStudent.objects.all()
+    context = {'students': students}
+    return render(request, 'active_student.html', context)
