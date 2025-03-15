05cd0f6ccaa4ec135941238b4a0bf9b7040d0db1
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-03-15 16:29:59 +0500

    faces_of_school

--- common/admin.py
+++ common/admin.py
@@ -19,38 +19,6 @@ class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     search_fields = ('title', 'description')
 
-
-@admin.register(models.Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'new', 'text')
-    list_filter = ('user',)
-
-    def has_add_permission(self, request):
-        return False  # Qo'shish (Create) bloklanadi
-
-    def has_change_permission(self, request, obj=None):
-        return False  # O'zgartirish (Update) bloklanadi
-
-    def has_delete_permission(self, request, obj=None):
-        return False  # O'chirish (Delete) bloklanadi
-
-
-@admin.register(models.Notifications)
-class NotificationsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'message', 'is_sent', 'user')
-    list_filter = ('is_sent', 'message')
-
-
-@admin.register(models.Test)
-class TestAdmin(admin.ModelAdmin):
-    list_display = ('id', 'subjects', 'class_choices')
-    list_filter = ('class_choices', 'subjects')
-
-@admin.register(models.Questions)
-class QuestionsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'test', 'true_answer', 'question_text')
-    list_filter = ('test',)
-
 @admin.register(models.ActiveStudent)
 class ActiveStudentAdmin(admin.ModelAdmin):
     list_display = ('id', 'full_name', 'class_name', 'image')
--- common/models.py
+++ common/models.py
@@ -42,66 +42,6 @@ class News(BaseModel):
         return f"{self.title}"
 
 
-class Comment(BaseModel):
-    ''' Comment model for news '''
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_user')
-    new = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comment_new')
-    text = models.TextField()
-
-    def __str__(self):
-        return f"{self.user} | {self.new}"
-
-
-class Notifications(BaseModel):
-    ''' Notification model for test and new '''
-    message = models.CharField(max_length=550)
-    link = models.CharField(max_length=255, null=True, blank=True)
-    is_sent = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='notification_user', null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.message}"
-
-
-class Test(BaseModel):
-    ''' Test model for school subjects '''
-    CLASS_CHOICES = [
-        ('5-CLASS', '5-sinf'),
-        ('6-CLASS', '6-sinf'),
-        ('7-CLASS', '7-sinf'),
-        ('8-CLASS', '8-sinf'),
-        ('9-CLASS', '9-sinf'),
-        ('10-CLASS', '10-sinf'),
-        ('11-CLASS', '11-sinf')
-    ]
-    subjects = models.ForeignKey(Subjects, on_delete=models.CASCADE, related_name='test_objects')
-    start_date = models.DateTimeField(default=None, null=True, blank=True)
-    end_date = models.DateTimeField(default=None, null=True, blank=True)
-    class_choices = models.CharField(choices=CLASS_CHOICES, max_length=10, default='5-CLASS')
-
-    def __str__(self):
-        return f"{self.class_choices}"
-
-
-class Questions(BaseModel):
-    ''' Questions model for test '''
-    TRUE_ANSWER_CHOICES = [
-        ('A', 'a'),
-        ('B', 'b'),
-        ('C', 'c'),
-        ('D', 'd')
-    ]
-    question_text = models.TextField()
-    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='question_test')
-    true_answer = models.CharField(choices=TRUE_ANSWER_CHOICES, max_length=2, default='A')
-    variant_a = models.CharField(max_length=255)
-    variant_b = models.CharField(max_length=255)
-    variant_c = models.CharField(max_length=255)
-    variant_d = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f" {self.test} | {self.true_answer}"
-
 
 class ActiveStudent(BaseModel):
     ''' Active students dict '''
